6458fab4d83dcd07d53346cd1f344d44c153e05f
Zahra Alizadeh <zahra.a@behinrahkar.com>
2024-01-09 22:26:31 +0330

    add book_id and quantity_available to AddNewBook class

--- schema/_input.py
+++ schema/_input.py
@@ -2,16 +2,19 @@ from pydantic import BaseModel
 
 
 class RegisterInput(BaseModel):
+    author_id: int
     name: str
     biography: str
 
 
 class AddNewBook(BaseModel):
+    book_id: int
     title: str
     author: str
     genre: str
     published_date: str
     isbn: str
+    quantity_available: int
 
 
 class UpdateDetailBooks(BaseModel):
