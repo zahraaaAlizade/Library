56caf68a6e64fc1ff3fefa46f679ad2e0e0d0c9a
Zahra Alizadeh <zahra.a@behinrahkar.com>
2024-01-10 20:13:11 +0330

    add DeleteSuccessful function

--- exeptions.py
+++ exeptions.py
@@ -11,3 +11,9 @@ class BooksNotFound(HTTPException):
     def __init__(self, route: str) -> None:
         self.status_code = status.HTTP_404_NOT_FOUND
         self.detail = f"Book Not Found Error In Route {route}"
+
+
+class DeleteSuccessful(HTTPException):
+    def __init__(self, route: str) -> None:
+        self.status_code = status.HTTP_200_OK
+        self.detail = f"Delete Book Successfully {route}"
